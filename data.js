8d63472864f87efe8e22fed9a0caf9adad707b43
yudithleon <yudithkleon@gmail.com>
2025-11-11 17:14:48 -0500

    cambios de integración

--- data.js
+++ data.js
@@ -1,109 +1,132 @@
 export const data = [
-        {
-        letra: "A",
-        URL: "https://i.pinimg.com/originals/6f/dd/4e/6fdd4e9b2e20105c88ddfd6c3f6c84be.png",
-        Name: "Avión "
-    }, {
-        letra: "B",
-        URL: "https://soundcloud.com/leptospilove_7/burro-do-shrek-freestyle.jpg",
-        Name: "Burro"
-    }, {
-        letra: "C",
-        URL: "https://revistadigital.mx/wp-content/uploads/2020/07/CP_0720_portada_BugsBunny.jpg",
-        Name: "conejo"
-    }, {
-        letra: "D",
-        URL: "https://share.google/images/1jVDnFDHO32tiXlEK",
-        Name: "   "
-    }, {
-        letra: "E",
-        URL: "https://share.google/images/YccljgVHgIUhL5Tgx",
-        Name: "   "
-    }, {
-        letra: "F",
-        URL: "https://share.google/images/NzwLDOaRd1XzjwoWo",
-        Name: "   "
-    }, {
-        letra: "G",
-        URL: "https://share.google/images/cM1xPqwOn9R3WgZTg",
-        Name: "   "
-    }, {
-        letra: "H",
-        URL: "https://share.google/images/L0Nkl7at2Eh53p6mO",
-        Name: "   "
-    }, {
-        letra: "I",
-        URL: "https://share.google/images/URuDqWWfJGbBTXL3g",
-        Name: "   "
-    }, {
-        letra: "J",
-        URL: "https://share.google/images/BxYaZJ7KCC9swCcqX",
-        Name: "   "
-    }, {
-        letra: "K",
-        URL: "https://share.google/images/mrdFpr4PgKUlqgnZt",
-        Name: "   "
-    }, {
-        letra: "L",
-        URL: "https://share.google/images/RWnG0C2YLnT51OD9W",
-        Name: "   "
-    }, {
-        letra: "M",
-        URL: "https://share.google/images/BEeYut7ukzXUZrsgx",
-        Name: "   "
-    }, {
-        letra: "N",
-        URL: "https://share.google/images/k2iTUXpyivuSHry48",
-        Name: "   "
-    }, {
-        letra: "O",
-        URL: "https://share.google/images/2sTA1kv7Bpo67KULC",
-        Name: "   "
-    }, {
-        letra: "P",
-        URL: "https://share.google/images/SCnANBGDhDS2COdsF",
-        Name: "   "
-    }, {
-        letra: "Q",
-        URL: "https://share.google/images/HGPaj8Go4cfyakvIx",
-        Name: "   "
-    }, {
-        letra: "R",
-        URL: "https://share.google/images/gIrdNu9sgMB1V7LqC",
-        Name: "   "
-    },{
-        letra: "S",
-        URL: "https://share.google/images/IqEonvMaH5MbwxdnF",
-        Name: "   "
-    },{
-        letra: "T",
-        URL: "https://share.google/images/pop4W7d7VQdy7JmeD",
-        Name: "   "
-    },{
-        letra: "U",
-        URL: "https://share.google/images/U5LlkO9Zvxfn108Ne",
-        Name: "   "
-    },{
-        letra: "V",
-        URL: "https://share.google/images/uwlUXHzqaMs37bpV6",
-        Name: "   "
-    },{
-        letra: "W",
-        URL: "https://share.google/images/EFSLbgP8OletrarAGY5G",
-        Name: "   "
-    },{
-        letra: "X",
-        URL: "https://share.google/images/60kOR5iWX358tW1Sf",
-        Name: "   "
-    },{
-        letra: "Y",
-        URL: "https://share.google/images/ya1PMHdAfs0BoLaoz",
-        Name: "   "
-    },{
-        letra: "Z",
-        URL: "https://share.google/images/fakgvOAjxJJSyoe0d",
-        Name: "   "
-    },
-
-
-]
+  {
+    letra: "A",
+    URL: "https://i.pinimg.com/originals/6f/dd/4e/6fdd4e9b2e20105c88ddfd6c3f6c84be.png",
+    Name: "Avión ",
+  },
+  {
+    letra: "B",
+    URL: "https://soundcloud.com/leptospilove_7/burro-do-shrek-freestyle.jpg",
+    Name: "Burro",
+  },
+  {
+    letra: "C",
+    URL: "https://revistadigital.mx/wp-content/uploads/2020/07/CP_0720_portada_BugsBunny.jpg",
+    Name: "conejo",
+  },
+  {
+    letra: "D",
+    URL: "https://share.google/images/1jVDnFDHO32tiXlEK",
+    Name: "   ",
+  },
+  {
+    letra: "E",
+    URL: "https://share.google/images/YccljgVHgIUhL5Tgx",
+    Name: "   ",
+  },
+  {
+    letra: "F",
+    URL: "https://share.google/images/NzwLDOaRd1XzjwoWo",
+    Name: "   ",
+  },
+  {
+    letra: "G",
+    URL: "https://share.google/images/cM1xPqwOn9R3WgZTg",
+    Name: "   ",
+  },
+  {
+    letra: "H",
+    URL: "https://share.google/images/L0Nkl7at2Eh53p6mO",
+    Name: "   ",
+  },
+  {
+    letra: "I",
+    URL: "https://share.google/images/URuDqWWfJGbBTXL3g",
+    Name: "   ",
+  },
+  {
+    letra: "J",
+    URL: "https://share.google/images/BxYaZJ7KCC9swCcqX",
+    Name: "   ",
+  },
+  {
+    letra: "K",
+    URL: "https://static.wixstatic.com/media/3d7178_0a85394d3a7d48d889c28b6f230985d9~mv2.jpeg/v1/fill/w_568,h_440,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/3d7178_0a85394d3a7d48d889c28b6f230985d9~mv2.jpeg",
+    Name: "   ",
+  },
+  {
+    letra: "L",
+    URL: "https://static.wixstatic.com/media/3d7178_0a85394d3a7d48d889c28b6f230985d9~mv2.jpeg/v1/fill/w_568,h_440,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/3d7178_0a85394d3a7d48d889c28b6f230985d9~mv2.jpeg",
+    Name: "   ",
+  },
+  {
+    letra: "M",
+    URL: "https://lenguaje.com/wp-content/uploads/2023/05/PALABRAS-CON-M.png",
+    Name: "   ",
+  },
+  {
+    letra: "N",
+    URL: "https://lenguaje.com/wp-content/uploads/2023/05/PALABRAS-CON-N-2.png",
+    Name: "   ",
+  },
+  {
+    letra: "O",
+    URL: "https://www.proferecursos.com/wp-content/uploads/Palabras-que-empiezan-por-O.jpg",
+    Name: "   ",
+  },
+  {
+    letra: "P",
+    URL: "https://lenguaje.com/wp-content/uploads/2023/05/PALABRAS-CON-P.png",
+    Name: "   ",
+  },
+  {
+    letra: "Q",
+    URL: "https://www.proferecursos.com/wp-content/uploads/Palabras-con-Q.jpg",
+    Name: "   ",
+  },
+  {
+    letra: "R",
+    URL: "https://share.google/images/gIrdNu9sgMB1V7LqC",
+    Name: "   ",
+  },
+  {
+    letra: "S",
+    URL: "https://share.google/images/IqEonvMaH5MbwxdnF",
+    Name: "   ",
+  },
+  {
+    letra: "T",
+    URL: "https://share.google/images/pop4W7d7VQdy7JmeD",
+    Name: "   ",
+  },
+  {
+    letra: "U",
+    URL: "https://share.google/images/U5LlkO9Zvxfn108Ne",
+    Name: "   ",
+  },
+  {
+    letra: "V",
+    URL: "https://share.google/images/uwlUXHzqaMs37bpV6",
+    Name: "   ",
+  },
+  {
+    letra: "W",
+    URL: "https://share.google/images/EFSLbgP8OletrarAGY5G",
+    Name: "   ",
+  },
+  {
+    letra: "X",
+    URL: "https://share.google/images/60kOR5iWX358tW1Sf",
+    Name: "   ",
+  },
+  {
+    letra: "Y",
+    URL: "https://share.google/images/ya1PMHdAfs0BoLaoz",
+    Name: "   ",
+  },
+  {
+    letra: "Z",
+    URL: "https://share.google/images/fakgvOAjxJJSyoe0d",
+    Name: "   ",
+  },
+];
