6af98d276dadaeb2ecd53e97f5ba3ed305c91a46
yudithleon <yudithkleon@gmail.com>
2025-11-11 16:40:30 -0500

    integracion de data + funcionabilidad

--- data.js
+++ data.js
@@ -1,107 +1,73 @@
 export const data = [
-    {
-       letra: "A",
-        URL: " https://i.pinimg.com/originals/6f/dd/4e/6fdd4e9b2e20105c88ddfd6c3f6c84be.png",
+        {
+        letra: "A",
+        URL: "https://i.pinimg.com/originals/6f/dd/4e/6fdd4e9b2e20105c88ddfd6c3f6c84be.png",
         Name: "Avión "
     }, {
-       letra: "B",
+        letra: "B",
         URL: "https://soundcloud.com/leptospilove_7/burro-do-shrek-freestyle",
         Name: "Burro"
     }, {
-       letra: "C",
+        letra: "C",
         URL: "https://revistadigital.mx/wp-content/uploads/2020/07/CP_0720_portada_BugsBunny.jpg",
         Name: "conejo"
     }, {
-       letra: "D",
-        URL: " https://share.google/images/1jVDnFDHO32tiXlEK",
+        letra: "D",
+        URL: "https://share.google/images/1jVDnFDHO32tiXlEK",
         Name: "   "
     }, {
-       letra: "E",
-        URL: "https://share.google/images/YccljgVHgIUhL5Tgx",
+        letra: "E",
+        URL: "https://share.google/images/YccljgVHgIUhL5Tgx ",
         Name: "   "
     }, {
-       letra: "F",
-        URL: " https://share.google/images/NzwLDOaRd1XzjwoWo",
+        letra: "F",
+        URL: "https://share.google/images/NzwLDOaRd1XzjwoWo",
         Name: "   "
     }, {
-       letra: "G",
+        letra: "G",
         URL: "https://share.google/images/cM1xPqwOn9R3WgZTg",
         Name: "   "
     }, {
-       letra: "H",
+        letra: "H",
         URL: "https://share.google/images/L0Nkl7at2Eh53p6mO",
         Name: "   "
     }, {
-       letra: "I",
+        letra: "I",
         URL: "https://share.google/images/URuDqWWfJGbBTXL3g",
         Name: "   "
     }, {
-       letra: "J",
+        letra: "J",
         URL: "https://share.google/images/BxYaZJ7KCC9swCcqX",
         Name: "   "
     }, {
-       letra: "K",
-        URL: "   ",
+        letra: "K",
+        URL: "https://share.google/images/mrdFpr4PgKUlqgnZt",
         Name: "   "
     }, {
-       letra: "L",
-        URL: "   ",
+        letra: "L",
+        URL: "https://share.google/images/RWnG0C2YLnT51OD9W",
         Name: "   "
     }, {
-       letra: "M",
-        URL: "   ",
+        letra: "M",
+        URL: "https://share.google/images/BEeYut7ukzXUZrsgx",
         Name: "   "
     }, {
-       letra: "N",
-        URL: "   ",
+        letra: "N",
+        URL: "https://share.google/images/k2iTUXpyivuSHry48",
         Name: "   "
     }, {
-       letra: "O",
-        URL: "   ",
+        letra: "O",
+        URL: "https://share.google/images/2sTA1kv7Bpo67KULC",
         Name: "   "
     }, {
-       letra: "P",
-        URL: "   ",
+        letra: "P",
+        URL: "https://share.google/images/SCnANBGDhDS2COdsF",
         Name: "   "
     }, {
-       letra: "Q",
-        URL: "   ",
+        letra: "Q",
+        URL: "https://share.google/images/HGPaj8Go4cfyakvIx",
         Name: "   "
-    }, {
-       letra: "R",
-        URL: "   ",
-        Name: "   "
-    }, {
-       letra: "S",
-        URL: "   ",
-        Name: "   "
-    }, {
-       letra: "T",
-        URL: "   ",
-        Name: "   "
-    },{
-       letra: "U",
-        URL: "   ",
-        Name: "   "
-    },{
-       letra: "V",
-        URL: "   ",
-        Name: "   "
-    },{
-       letra: "W",
-        URL: "   ",
-        Name: "   "
-    },{
-       letra: "X",
-        URL: "   ",
-        Name: "   "
-    },{
-       letra: "Y",
-        URL: "   ",
-        Name: "   "
-    },{
-       letra: "Z",
-        URL: "   ",
-        Name: "   "
-    },
+    }, 
+
+
 ]
